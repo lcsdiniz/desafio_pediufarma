668770a601baedac69a0e7abcccf8fef9115d028
lcsdiniz <lucasdiniz.smt@gmail.com>
2020-05-21 11:55:07 -0300

    Minor changes

--- main.py
+++ main.py
@@ -1,31 +1,34 @@
 import mysql.connector, json
 
-cnx = mysql.connector.connect(user='root', 
-                              password='',
-                              host='127.0.0.1',
-                              database='sys')
+def dbConnection():
+  connection = mysql.connector.connect(user='root', 
+                                      password='',
+                                      host='127.0.0.1',
+                                      database='sys')
+  cursor = connection.cursor()
 
-cursor = cnx.cursor()
+  query = ('''SELECT CASE
+    WHEN validade IS NULL THEN pmc
+    WHEN promocao IS NULL THEN pmc
+    WHEN validade >= curdate() THEN promocao
+    WHEN validade < curdate() THEN IF(pmc IS NULL, promocao, pmc)
+    END AS precoFinal, barra, quantidade FROM sys.estoque WHERE pmc IS NOT NULL AND quantidade>0''')
 
-query = ('''SELECT CASE
-	WHEN validade IS NULL THEN pmc
-  WHEN promocao IS NULL THEN pmc
-  WHEN validade >= curdate() THEN promocao
-  WHEN validade < curdate() THEN IF(pmc IS NULL, promocao, pmc)
-  END AS precoFinal, barra, quantidade FROM sys.estoque WHERE pmc IS NOT NULL AND quantidade>0''')
+  cursor.execute(query)
+  result = cursor.fetchall()
+  cursor.close()
+  connection.close()
+  return result
 
-cursor.execute(query)
+def jsonParser(queryResult):
+  data = []
+  for (precoFinal, barra, quantidade) in queryResult:
+    data.append(dict([("ean", barra),("preco", precoFinal),("estoque", quantidade)]))
 
-data = []
+  json_object = json.dumps(data, indent = 2)
 
-for (precoFinal, barra, quantidade) in cursor:
-  data.append(dict([("ean", barra),("preco", precoFinal),("estoque", quantidade)]))
+  with open("sample.json", "w") as outfile: 
+      outfile.write(json_object) 
 
-json_object = json.dumps(data, indent = 2) 
-
-with open("sample.json", "w") as outfile: 
-    outfile.write(json_object) 
-
-cursor.close()
-
-cnx.close()
+queryResult = dbConnection()
+jsonParser(queryResult)
